212b989244129c9bed057527eb010bf77123eb04
Anton Drozd <anton.drozd.95@gmail.com>
2019-04-25 20:50:05 +0200

    Add error handling in withDataFetching HOC

--- src/hocs/withDataFetching.js
+++ src/hocs/withDataFetching.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react'
 
 import Spinner from '../components/spinner'
+import ErrorIndicator from '../components/error-indicator'
 
 import { getDisplayName } from './helpers'
 
 const withDataFetching = View => {
   class WithDataFetching extends Component {
     state = {
-      data: null
+      data: null,
+      hasError: false
     }
 
     componentDidMount() {
-      this.props.getData().then(data => this.setState({ data }))
+      this.props
+        .getData()
+        .then(data => this.setState({ data, hasError: false }))
+        .catch(error => {
+          this.setState({ hasError: true })
+          console.error(error)
+        })
     }
 
     render() {
-      const { data } = this.state
+      const { data, hasError } = this.state
+
+      if (hasError) return <ErrorIndicator />
 
       if (!data) return <Spinner />
 
